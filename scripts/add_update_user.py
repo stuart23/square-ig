2768c1bbcf1940645a3f6153ba25930355c35671
Stuart Buckingham <stuart.buckingham@gmail.com>
2024-08-27 18:20:36 -0500

    Handler and code version

--- scripts/add_update_user.py
+++ scripts/add_update_user.py
@@ -1,7 +1,10 @@
 
 def handler(event, context):
-    square_event_type = event['type']
-    data = event['data']
+    try:
+        square_event_type = event['type']
+        data = event['data']
+    except KeyError:
+        raise KeyError('Could not parse event: ', event)
     if data['type'] != 'customer':
         raise TypeError('Received non customer data type.')
     id = data['id']
