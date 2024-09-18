e65a1cbece57ccc19a22056d72e1461aed364345
Stuart Buckingham <stuart.buckingham@gmail.com>
2024-09-18 13:26:26 -0500

    Try hardcoding

--- full-stack/lambda.tf
+++ full-stack/lambda.tf
@@ -8,7 +8,8 @@ resource "aws_lambda_function" "add_user" {
   description   = "Deploys DAGs in S3 bucket to Astro"
   package_type  = "Image"
   architectures = ["arm64"]
-  image_uri     = var.lambda_image
+  image_uri     = "015140017687.dkr.ecr.us-east-1.amazonaws.com/lambdaimage@sha256:9372e5dcca5d63b4fd59e6cfe61d223bd1cccf1d408bb3ef06e4b08f916ff29b"
+  # image_uri     = var.lambda_image
   role          = aws_iam_role.lambda_role.arn
   timeout       = 30
   memory_size   = 256
