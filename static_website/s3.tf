6fc46ca15e57a8f2e533c9d445920c9fd4985fbf
Stuart Buckingham <stuart.buckingham@gmail.com>
2024-09-18 18:46:55 -0500

    remove depends_on

--- static_website/s3.tf
+++ static_website/s3.tf
@@ -6,7 +6,6 @@ resource "aws_s3_bucket" "website_bucket" {
 resource "aws_s3_bucket_acl" "website_bucket_acl" {
   bucket     = aws_s3_bucket.website_bucket
   acl        = "public-read"
-  depends_on = [aws_s3_bucket_ownership_controls.s3_bucket_acl_ownership]
 }
 
 
