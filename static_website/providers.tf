1340d1c968301dd8e16065527270dfd2b8a3f5de
Stuart Buckingham <stuart.buckingham@gmail.com>
2024-09-18 18:45:56 -0500

    missing providers

---
+++ static_website/providers.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = "~> 1.9.5"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-west-1"
+}
