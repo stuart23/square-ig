84c08cd82bbe48666fb8007aaf4df33c1c27c8ed
Stuart Buckingham <stuart.buckingham@gmail.com>
2024-09-18 19:24:37 -0500

    typo

--- static_website/s3.tf
+++ static_website/s3.tf
@@ -22,7 +22,7 @@ resource "aws_s3_bucket_public_access_block" "website_bucket_acl" {
 
 
 resource "aws_s3_bucket_ownership_controls" "website_bucket_ownership" {
-  bucket = aws_s3_bucket.example.id
+  bucket = aws_s3_bucket.website_bucket.id
   rule {
     object_ownership = "BucketOwnerPreferred"
   }
