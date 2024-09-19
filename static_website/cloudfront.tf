ddcca72ecd6a5889b6c689cde36288f18f3423de
Stuart Buckingham <stuart.buckingham@gmail.com>
2024-09-18 20:27:28 -0500

    sni-only

--- static_website/cloudfront.tf
+++ static_website/cloudfront.tf
@@ -46,6 +46,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
 
   viewer_certificate {
     acm_certificate_arn = aws_acm_certificate.certificate.arn
+    ssl_support_method = "sni-only"
   }
 }
 
