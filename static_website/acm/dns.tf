257c39b4080ae07ddf355b56f7d09bba17f8309d
Stuart Buckingham <stuart.buckingham@gmail.com>
2024-09-19 12:37:00 -0500

    Name

--- static_website/acm/dns.tf
+++ static_website/acm/dns.tf
@@ -1,8 +1,8 @@
 resource "porkbun_dns_record" "main" {
-    for_each = aws_acm_certificate.certificate.domain_validation_options 
+  for_each = aws_acm_certificate.certificate.domain_validation_options
 
-    domain  = each.key
-    name   = each.value.resource_record_name
-    record = each.value.resource_record_value
-    type   = each.value.resource_record_type
+  domain  = each.key
+  name    = each.value.resource_record_name
+  content = each.value.resource_record_value
+  type    = each.value.resource_record_type
 }
