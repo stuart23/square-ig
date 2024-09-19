5ee0f3c35c128a2f32118af4d7dc2e12e33888c1
Stuart Buckingham <stuart.buckingham@gmail.com>
2024-09-19 16:58:22 -0500

    Test

--- static_website/cloudfront.tf
+++ static_website/cloudfront.tf
@@ -1,55 +1,56 @@
-# resource "aws_cloudfront_distribution" "s3_distribution" {
-#   origin {
-#     domain_name = aws_s3_bucket.website_bucket.bucket_regional_domain_name
-#     origin_id   = var.domain_name
-#   }
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.website_bucket.bucket_regional_domain_name
+    origin_id   = var.domain_name
+  }
 
-#   enabled             = true
-#   is_ipv6_enabled     = true
-#   default_root_object = "index.html"
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
 
-#   # logging_config {
-#   #   include_cookies = false
-#   #   bucket          = "mylogs.s3.amazonaws.com"
-#   #   prefix          = "myprefix"
-#   # }
+  # logging_config {
+  #   include_cookies = false
+  #   bucket          = "mylogs.s3.amazonaws.com"
+  #   prefix          = "myprefix"
+  # }
 
-#   aliases = ["plantsocietyatx.com", "theplantsocietyatx.com"]
+#   aliases = aws_acm_certificate_validation.certificate_validation
+  aliases = [ module.acm.aws_acm_certificate.certificate.domain_name ]
 
-#   default_cache_behavior {
-#     allowed_methods  = ["GET", "HEAD"]
-#     cached_methods   = ["GET", "HEAD"]
-#     target_origin_id = var.domain_name
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = var.domain_name
 
-#     forwarded_values {
-#       query_string = false
+    forwarded_values {
+      query_string = false
 
-#       cookies {
-#         forward = "none"
-#       }
-#     }
+      cookies {
+        forward = "none"
+      }
+    }
 
-#     viewer_protocol_policy = "redirect-to-https"
-#     min_ttl                = 0
-#     default_ttl            = 3600
-#     max_ttl                = 86400
-#   }
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
 
-#   price_class = "PriceClass_100"
+  price_class = "PriceClass_100"
 
-#   restrictions {
-#     geo_restriction {
-#       restriction_type = "none"
-#       locations        = []
-#     }
-#   }
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+      locations        = []
+    }
+  }
 
-#   viewer_certificate {
-#     acm_certificate_arn = aws_acm_certificate.certificate.arn
-#     ssl_support_method = "sni-only"
-#   }
+  viewer_certificate {
+    acm_certificate_arn = aws_acm_certificate.certificate.arn
+    ssl_support_method = "sni-only"
+  }
+}
+
+# output "cloudfront_distribution_url" {
+#   value = aws_cloudfront_distribution.s3_distribution.
 # }
-
-# # output "cloudfront_distribution_url" {
-# #   value = aws_cloudfront_distribution.s3_distribution.
-# # }
