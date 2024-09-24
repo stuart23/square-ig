0f0d69af7aaad190956fc1bc6db3e5021ab0647b
Stuart Buckingham <stuart.buckingham@gmail.com>
2024-09-24 14:50:14 -0500

    Remove output

--- static_website/s3.tf
+++ static_website/s3.tf
@@ -71,6 +71,6 @@
 # }
 
 
-output "s3_bucket_id" {
-  value = aws_s3_bucket_website_configuration.website_bucket_config.website_endpoint
-}
+# output "s3_bucket_id" {
+#   value = aws_s3_bucket_website_configuration.website_bucket_config.website_endpoint
+# }
