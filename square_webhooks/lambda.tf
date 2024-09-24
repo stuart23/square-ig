c88e53d64578ff7d039c3c83836c84d32038667d
Stuart Buckingham <stuart.buckingham@gmail.com>
2024-09-24 16:22:41 -0500

    Change handler to add_user

--- square_webhooks/lambda.tf
+++ square_webhooks/lambda.tf
@@ -12,6 +12,7 @@ resource "aws_lambda_function" "add_user" {
   role          = aws_iam_role.lambda_role.arn
   timeout       = 30
   memory_size   = 256
+  handler       = "add_user.handler"
   logging_config {
     log_group  = aws_cloudwatch_log_group.add_user_lambda_logs.name
     log_format = "Text"
