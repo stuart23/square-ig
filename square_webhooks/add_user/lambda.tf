e969d0ad5904495aa601a3c7780c67213d489311
Stuart Buckingham <stuart.buckingham@gmail.com>
2024-09-25 01:57:27 -0500

    Turn back on

--- square_webhooks/add_user/lambda.tf
+++ square_webhooks/add_user/lambda.tf
@@ -1,30 +1,30 @@
-# resource "aws_cloudwatch_log_group" "add_user_lambda_logs" {
-#   name              = "add_user_lambda"
-#   retention_in_days = 14
-# }
+resource "aws_cloudwatch_log_group" "add_user_lambda_logs" {
+  name              = "add_user_lambda"
+  retention_in_days = 14
+}
 
-# resource "aws_lambda_function" "add_user" {
-#   function_name = "add_user"
-#   description   = "Deploys DAGs in S3 bucket to Astro"
-#   package_type  = "Image"
-#   architectures = ["arm64"]
-#   image_uri     = var.lambda_image
-#   role          = var.lambda_role_arn
-#   timeout       = 30
-#   memory_size   = 256
-#   image_config {
-#     command = ["add_user.handler"]
-#   }
-#   logging_config {
-#     log_group  = aws_cloudwatch_log_group.add_user_lambda_logs.name
-#     log_format = "Text"
-#   }
-#   # environment {
-#   #   variables = {
-#   #     token = token
-#   #   }
-#   # }
-#   ephemeral_storage {
-#     size = 1024 # Min 512 MB and the Max 10240 MB
-#   }
-# }
+resource "aws_lambda_function" "add_user" {
+  function_name = "add_user"
+  description   = "Deploys DAGs in S3 bucket to Astro"
+  package_type  = "Image"
+  architectures = ["arm64"]
+  image_uri     = var.lambda_image
+  role          = var.lambda_role_arn
+  timeout       = 30
+  memory_size   = 256
+  image_config {
+    command = ["add_user.handler"]
+  }
+  logging_config {
+    log_group  = aws_cloudwatch_log_group.add_user_lambda_logs.name
+    log_format = "Text"
+  }
+  # environment {
+  #   variables = {
+  #     token = token
+  #   }
+  # }
+  ephemeral_storage {
+    size = 1024 # Min 512 MB and the Max 10240 MB
+  }
+}
