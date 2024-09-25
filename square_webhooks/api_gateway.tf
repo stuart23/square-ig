6b5f4ea5dbfd71b4d8b2aa9a7ee4815788c92301
Stuart Buckingham <stuart.buckingham@gmail.com>
2024-09-25 13:30:18 -0500

    Add endpoint

--- square_webhooks/api_gateway.tf
+++ square_webhooks/api_gateway.tf
@@ -3,3 +3,8 @@ resource "aws_apigatewayv2_api" "square_gateway" {
   description   = "Endpoint for Square webhooks."
   protocol_type = "HTTP"
 }
+
+output "gateway_endpoint" {
+  value = aws_apigatewayv2_api.square_gateway.api_endpoint
+  description = "Endpoint of the square gateway API. Individual endpoints (e.g. /add_user) are appended to this."
+}
