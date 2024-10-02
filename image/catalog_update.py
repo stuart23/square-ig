e419386bc0176d6a188ba91d13f64f556b8fbbc4
Stuart Buckingham <stuart.buckingham@gmail.com>
2024-10-02 00:25:24 -0500

    attempted relative import with no known parent package

--- image/catalog_update.py
+++ image/catalog_update.py
@@ -1,6 +1,6 @@
 from boto3 import client as Boto3Client
 from json import loads
-from .square_client import get_all_catalog_objects
+from square_client import get_all_catalog_objects
 
 
 def handler(event, context):
