9b15bc7a98e73a7886cd125288f55a6452c1f472
Stuart Buckingham <stuart.buckingham@gmail.com>
2024-10-02 00:18:02 -0500

    Query square objects

--- image/catalog_update.py
+++ image/catalog_update.py
@@ -1,7 +1,9 @@
 from boto3 import client as Boto3Client
 from json import loads
+from .square_client import get_all_catalog_objects
 
 
 def handler(event, context):
-    print('Hello from the catalog Update')
-    print(event)
+    objects = get_all_catalog_objects()
+    print(objects)
+
---
+++ image/square_client.py
@@ -0,0 +1,43 @@
+from square.http.auth.o_auth_2 import BearerAuthCredentials
+from square.client import Client as SquareClient
+from boto3 import client as Boto3Client
+
+SECRET_NAME = "square_token"
+
+boto3_client = Boto3Client('secretsmanager')
+square_token = boto3_client.get_secret_value(SECRET_NAME)
+
+client = SquareClient(
+    bearer_auth_credentials=BearerAuthCredentials(
+        access_token=square_token
+    ),
+    environment='Production'
+)
+
+
+def get_all_catalog_objects():
+"""Retrieves all catalog objects using pagination."""
+
+    catalog_api = client.catalog_api
+    cursor = None
+    objects = []
+
+    while True:
+        response = catalog_api.search_catalog_objects(
+            body={
+                "cursor": cursor,
+                "limit": 100  # Adjust limit as needed
+            }
+        )
+
+        if response.is_success():
+            objects.extend(response.body["objects"])
+            cursor = response.body.get("cursor")
+
+            if cursor is None:
+                break  # No more pages
+        else:
+            print(f"Error: {response.errors}")
+            break  # Stop on error
+
+    return objects
