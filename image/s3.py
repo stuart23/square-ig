176e7d29d024592840c22886486a5c9fc7a72070
Stuart Buckingham <stuart.buckingham@gmail.com>
2024-10-02 22:38:32 -0500

    Specify format

--- image/s3.py
+++ image/s3.py
@@ -1,17 +1,18 @@
-import io
+from io import BytesIO
 from boto3 import client as Boto3Client
 from os import getenv
 
 
 s3client = Boto3Client('s3')
 BUCKET_NAME = getenv('catalog_bucket_name')
+FORMAT = 'png'
 
 
 def write_image(image, key):
     print(f"Writing object, {key} to bucket {BUCKET_NAME} in format {image.format}")
     # Save the image to an in-memory file
-    in_mem_file = io.BytesIO()
-    image.save(in_mem_file, format=image.format)
+    in_mem_file = BytesIO()
+    image.save(in_mem_file, format=FORMAT)
     in_mem_file.seek(0)
 
     # Upload image to s3
