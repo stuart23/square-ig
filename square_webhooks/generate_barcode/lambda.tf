fdf458c1ea406eb2047bdb0074b919bb2031d68f
Stuart Buckingham <stuart.buckingham@gmail.com>
2024-10-04 17:09:25 -0500

    FunctionName

--- square_webhooks/generate_barcode/lambda.tf
+++ square_webhooks/generate_barcode/lambda.tf
@@ -46,7 +46,7 @@ resource "aws_cloudwatch_metric_alarm" "generate_barcode_failure_alarm" {
   namespace         = "AWS/Lambda"
   metric_name       = "Errors"
   dimensions = {
-    Resource = aws_lambda_function.generate_barcode.name
+    FunctionName = aws_lambda_function.generate_barcode.function_name
   }
   comparison_operator = "GreaterThanThreshold"
   statistic           = "Maximum"
