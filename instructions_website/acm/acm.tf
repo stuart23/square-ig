beda4b31f1adef9b7bda33c5441ca66f29f6791b
Stuart Buckingham <stuart.buckingham@gmail.com>
2024-10-28 20:11:27 -0500

    acm

--- instructions_website/acm/acm.tf
+++ instructions_website/acm/acm.tf
@@ -17,7 +17,7 @@ resource "aws_acm_certificate" "certificate" {
 
 resource "aws_acm_certificate_validation" "certificate_validation" {
   certificate_arn         = aws_acm_certificate.certificate.arn
-  validation_record_fqdns = [for record in porkbun_dns_record.main : record.domain]
+  validation_record_fqdns = [for record in porkbun_dns_record.certificate_validation : record.domain]
 }
 
 output "fqdns" {
