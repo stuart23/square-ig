02d15e74e46d5063b0f4ad6e1d3884c9cfb074ed
Stuart Buckingham <stuart.buckingham@gmail.com>
2024-10-30 13:34:27 -0500

    Downscale provisioned

--- square_webhooks/catalog.tf
+++ square_webhooks/catalog.tf
@@ -1,8 +1,8 @@
 resource "aws_dynamodb_table" "catalog" {
   name           = "catalog"
   billing_mode   = "PROVISIONED"
-  read_capacity  = 25
-  write_capacity = 25
+  read_capacity  = 10
+  write_capacity = 10
   hash_key       = "SKU"
 
   attribute {
@@ -24,11 +24,15 @@ resource "aws_dynamodb_table" "catalog" {
     name               = "websiteIndex"
     hash_key           = "website"
     projection_type    = "ALL"
+    read_capacity      = 5
+    write_capacity     = 5
   }
 
   global_secondary_index {
     name               = "labelIndex"
     hash_key           = "label"
     projection_type    = "ALL"
+    read_capacity      = 5
+    write_capacity     = 5
   }
 }
