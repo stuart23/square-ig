6c848407512e0851b89a71c8690c7d2d3b110bd7
Stuart Buckingham <stuart.buckingham@gmail.com>
2024-10-31 20:48:45 -0500

    Tool now does it in batch

--- tools/delete_square_images.py
+++ tools/delete_square_images.py
@@ -44,22 +44,47 @@ def get_all_catalog_items():
     return objects
 
 
+def batch_filter_images(object_ids):
+    to_delete = []
+    response = catalog.batch_retrieve_catalog_objects(
+        body={
+            "object_ids":object_ids,
+            "include_related_objects":False,
+            "catalog_version":None,
+            "include_category_path_to_root":False
+        }
+    )
+    objects = response.body['objects']
+    for image in objects:
+        if image['image_data'].get('caption') == 'QR Code':
+            to_delete.append(image['id'])
+    print(f'Deleting {len(to_delete)} images')
+    catalog.batch_delete_catalog_objects(body={"object_ids":to_delete})
+
+
 if __name__ == '__main__':
     items = get_all_catalog_items()
     catalog = get_square_client().catalog
 
     for item in items:
+        print(item['item_data'])
         images = item['item_data'].get('image_ids', [])
+        print(f'Number of images: {len(images)}')
+        object_ids = []
+        to_delete = []
         for image_id in images:
-            image = catalog.retrieve_catalog_object(
-                object_id=image_id,
-                include_related_objects=False,
-                catalog_version=None,
-                include_category_path_to_root=False
-            )
-            try:
-                caption = image.body['object']['image_data']['caption']
-            except KeyError:
-                continue
-            if caption == 'QR Code':
-                catalog.delete_catalog_object(object_id=image_id)
+            object_ids.append(image_id)
+            if len(object_ids) >= 50:
+                batch_filter_images(object_ids)
+                object_ids = []
+                to_delete = []
+        else:
+            batch_filter_images(object_ids)
+
+
+            # image = catalog.retrieve_catalog_object(
+            #     object_id=image_id,
+            #     include_related_objects=False,
+            #     catalog_version=None,
+            #     include_category_path_to_root=False
+            # )
