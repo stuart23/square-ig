dcd95a0ec30e67c1416b56debf04283445958056
Stuart Buckingham <stuart.buckingham@gmail.com>
2024-11-01 02:26:03 -0500

    data

--- square_webhooks/gdrive.tf
+++ square_webhooks/gdrive.tf
@@ -2,6 +2,6 @@ data "google_project" "plantsociety_project" {}
 
 
 resource "google_project_service" "cloudapis" {
-  project = google_project.plantsociety_project.project_id
+  project = data.plantsociety_project.project_id
   service = "drive.googleapis.com"
 }
