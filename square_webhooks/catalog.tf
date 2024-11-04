46615a2790d0240479aa70e7a848428388a4f49c
Stuart Buckingham <stuart.buckingham@gmail.com>
2024-11-04 14:16:04 -0600

    Change PK

--- square_webhooks/catalog.tf
+++ square_webhooks/catalog.tf
@@ -3,7 +3,12 @@ resource "aws_dynamodb_table" "catalog" {
   billing_mode   = "PROVISIONED"
   read_capacity  = 10
   write_capacity = 10
-  hash_key       = "SKU"
+  hash_key       = "variation_id"
+
+  attribute {
+    name = "variation_id"
+    type = "S"
+  }
 
   attribute {
     name = "SKU"
@@ -21,18 +26,26 @@ resource "aws_dynamodb_table" "catalog" {
   }
 
   global_secondary_index {
+    name            = "skuIndex"
+    hash_key        = "sku"
+    projection_type = "ALL"
+    read_capacity   = 2
+    write_capacity  = 2
+  }
+
+  global_secondary_index {
     name            = "websiteIndex"
     hash_key        = "website"
     projection_type = "ALL"
-    read_capacity   = 5
-    write_capacity  = 5
+    read_capacity   = 2
+    write_capacity  = 2
   }
 
   global_secondary_index {
     name            = "labelIndex"
     hash_key        = "label"
     projection_type = "ALL"
-    read_capacity   = 5
-    write_capacity  = 5
+    read_capacity   = 2
+    write_capacity  = 2
   }
 }
