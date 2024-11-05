e68ef4de080a158f43e5c1edfd83c0dd5467328a
Stuart Buckingham <stuart.buckingham@gmail.com>
2024-11-05 00:16:54 -0600

    Update perms

--- square_webhooks/catalog_update/sqs.tf
+++ square_webhooks/catalog_update/sqs.tf
@@ -40,3 +40,8 @@ resource "aws_iam_role_policy_attachment" "gateway_sqs_write" {
   role       = aws_iam_role.gateway_sqs_write.name
   policy_arn = aws_iam_policy.sqs_write.arn
 }
+
+resource "aws_iam_role_policy_attachment" "push_logs" {
+  role       = aws_iam_role.gateway_sqs_write.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonAPIGatewayPushToCloudWatchLogs"
+}
