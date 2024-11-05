7f63b6935336a5fc18d10635ebdf95b786f9ad62
Stuart Buckingham <stuart.buckingham@gmail.com>
2024-11-04 23:44:38 -0600

    Deduplicate

--- square_webhooks/catalog_update/api_gateway.tf
+++ square_webhooks/catalog_update/api_gateway.tf
@@ -1,14 +1,4 @@
 resource "aws_apigatewayv2_integration" "catalog_update" {
-  api_id           = var.square_gateway_id
-  integration_type = "AWS_PROXY"
-
-  description            = "Webhook triggered when the catalog is updated"
-  integration_method     = "POST"
-  integration_uri        = aws_lambda_function.catalog_update.invoke_arn
-  payload_format_version = "2.0"
-}
-
-resource "aws_apigatewayv2_integration" "catalog_update" {
   api_id              = var.square_gateway_id
   credentials_arn     = aws_iam_role.gateway_sqs_write.arn
   description         = "SQS for messages to update catalog"
