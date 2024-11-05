580355caf35ec1623918e4fd151ca314adcbe956
Stuart Buckingham <stuart.buckingham@gmail.com>
2024-11-05 00:26:01 -0600

    Trigger lambda

--- square_webhooks/catalog_update/sqs.tf
+++ square_webhooks/catalog_update/sqs.tf
@@ -41,7 +41,14 @@ resource "aws_iam_role_policy_attachment" "gateway_sqs_write" {
   policy_arn = aws_iam_policy.sqs_write.arn
 }
 
+
 resource "aws_iam_role_policy_attachment" "push_logs" {
   role       = aws_iam_role.gateway_sqs_write.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonAPIGatewayPushToCloudWatchLogs"
+}
+
+
+resource "aws_lambda_event_source_mapping" "trigger_lambda" {
+  event_source_arn = aws_sqs_queue.catalog_update.arn
+  function_name    = aws_lambda_function.catalog_update.arn
 }
