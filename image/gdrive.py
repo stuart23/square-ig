57c257cc9eae89c68cac2662b9c7ebc8e7315c7a
Stuart Buckingham <stuart.buckingham@gmail.com>
2024-11-07 00:44:55 -0600

    Test upload

---
+++ image/gdrive.py
@@ -0,0 +1,23 @@
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+from os import getenv
+
+
+DRIVE_ID_ENV_VAR = "labels_google_drive_id"
+
+def writeFile():
+    drive_id = getenv(DRIVE_ID_ENV_VAR)
+    if not drive_id:
+        raise Exception('Drive ID Env Var not defined.')
+
+    service = build("drive", "v3")
+    
+    file_metadata = {"name": "pet_safe.png"}
+    media = MediaFileUpload("labels/assets/pet_safe.png", mimetype="image/png")
+
+    file = (
+        service.files()
+        .create(body=file_metadata, media_body=media, fields="id", driveId=drive_id)
+        .execute()
+    )
--- image/test.py
+++ image/test.py
@@ -1,22 +1,7 @@
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
-
-drive_name = 'labels'
-
+from .gdrive import writeFile
 def handler(event, context):
 
-    service = build("drive", "v3")
-
-    try:
-        results = service.drives().list().execute()
-    except HttpError as error:
-        print('Could not retrieve list of drives.')
-        raise error
-    drives = results['drives']
-    target_drives = [x for x in drives if x['name'] == drive_name]
-    if len(target_drives) != 1:
-        raise ValueError(f'Could not find drive \"{drive_name}\" or duplicate drive names in {drives}')
-    print(f'drive_id={target_drives[0]["id"]}')
+    writeFile()
     
 if __name__ == '__main__':
     handler(None, None)
