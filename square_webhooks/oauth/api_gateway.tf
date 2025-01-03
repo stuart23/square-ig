81c5c337e81adf88fd05d6d0fa50fd347ee17216
Stuart Buckingham <stuart.buckingham@gmail.com>
2025-01-03 00:08:38 -0600

    Endpoint invocation

--- square_webhooks/oauth/api_gateway.tf
+++ square_webhooks/oauth/api_gateway.tf
@@ -2,6 +2,10 @@ resource "aws_apigatewayv2_integration" "oauth" {
   api_id              = var.square_gateway_id
   description         = "oauth redirect URL"
   integration_type    = "AWS_PROXY"
+
+  integration_method     = "POST"
+  integration_uri        = aws_lambda_function.oauth.invoke_arn
+  payload_format_version = "2.0"
 }
 
 resource "aws_apigatewayv2_route" "oauth" {
