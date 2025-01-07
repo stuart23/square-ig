f0f9824eca6232768227c4c332af914c4b36cfc3
Stuart Buckingham <stuart.buckingham@gmail.com>
2025-01-07 15:25:27 -0600

    Return from callback

--- image/auth_callback_lambda.py
+++ image/auth_callback_lambda.py
@@ -10,11 +10,24 @@ def handler(event, context):
     if error := queryStringParameters.get('error'):
         error_description = queryStringParameters.get('error_description')
         print(f'OAUTH ERROR: {error} - {error_description}')
+        return {
+            'statusCode': 500,
+            'body': {'error': error, 'error_description': error_description}
+        }
     elif code := queryStringParameters.get('code'):
         publish({
             'code': code,
             'state': queryStringParameters.get('state'),
             'response_type': queryStringParameters.get('response_type'),
         })
+        return {
+            'statusCode' : 200,
+            'body': "Event Handled"
+        }
+
     else:
-        raise Exception('Not sure')
+        print(f'Square response unknown. Event details: f{event}')
+        return {
+            'statusCode' : 400,
+            'body': "Square did not return an expected response."
+        }
