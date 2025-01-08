eb8c1b451a3359e5c528c750f8504855e5ab892c
Stuart Buckingham <stuart.buckingham@gmail.com>
2025-01-08 00:27:02 -0600

    Try again

--- square_webhooks/auth/auth_handler_lambda.tf
+++ square_webhooks/auth/auth_handler_lambda.tf
@@ -23,7 +23,7 @@ resource "aws_iam_role_policy_attachment" "auth_handler_tenants_access_policy_at
 
 
 resource "aws_iam_role_policy_attachment" "auth_handler_secret_access_policy_attachment" {
-  role       = aws_iam_role.lambda_role.name
+  role       = aws_iam_role.auth_handler.name
   policy_arn = aws_iam_policy.read_square_credentials_secret.arn
 }
 
