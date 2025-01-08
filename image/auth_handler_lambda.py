b122f44d64af4a414634568e68ede40bc3606eac
Stuart Buckingham <stuart.buckingham@gmail.com>
2025-01-07 22:13:01 -0600

    new_connection WIP

--- image/auth_handler_lambda.py
+++ image/auth_handler_lambda.py
@@ -1,7 +1,15 @@
-from auth.token_queue import publish
+from auth import TenantClient
+from utils import SQSDeserialize
 
 def handler(event, context):
-    print('EVENT')
-    print(event)
-    print('CONTEXT')
-    print(context)
+    for body in SQSDeserialize(event):
+        action = body['action']
+        if action == 'new_connection':
+            new_connection(**body['data'])
+        else:
+            raise ValueError(f'No function for action {action}')
+
+
+def new_connection(**args):
+    tenant_client = TenantClient()
+    tenant_client.upsert_tenant(args)
---
+++ image/utils/sqs_deserialize.py
@@ -0,0 +1,22 @@
+from json import loads
+from json.decoder import JSONDecodeError
+
+
+def SQSDeserialize(event):
+    '''
+    Used to deserialize events from SQS. Provides some error checking.
+    '''
+    for record in event['Records']:
+        messageId = record["messageId"]
+        print(f'Processing messageId: {messageId}')
+
+        body = record["body"]
+        try:
+            deserialized_body = loads(body)
+        except TypeError as e:
+            e.add_note(f'Message body was not a string. Message body received is {body}')
+            raise e
+        except JSONDecodeError as e:
+            e.add_note(f'Could not decode json. Message body received is {body}')
+            raise e
+        yield deserialized_body
