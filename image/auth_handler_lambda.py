4c3b5b03851109f044872a0ea56082752bc38fe6
Stuart Buckingham <stuart.buckingham@gmail.com>
2025-01-09 15:45:47 -0600

    Refresh scheduler

--- image/auth_handler_lambda.py
+++ image/auth_handler_lambda.py
@@ -6,10 +6,20 @@ from utils import SQSDeserialize
 
 
 def handler(event, context):
+    '''
+    All messages on the auth SQS topic will trigger this lambda. Depending
+    on what is in the message, a different function will be launched.
+    Current actions are:
+    - new_connection: Triggered when a new oauth connection is made.
+    - find_tokens_to_refresh: Triggered on the aws eventbridge scheduler to
+        periodically check what tokens need to be refreshed.
+    '''
     for body in SQSDeserialize(event):
         action = body["action"]
         if action == "new_connection":
             new_connection(**body["data"])
+        elif action == "find_tokens_to_refresh":
+            find_tokens_to_refresh()
         else:
             raise ValueError(f"No function for action {action}")
 
@@ -44,3 +54,11 @@ def new_connection(**args):
         merchant_id=token_details["merchant_id"],
         refresh_token=token_details["refresh_token"],
     )
+
+
+def find_tokens_to_refresh():
+    '''
+    Reads the database to see if there are any tokens that need to be
+    refreshed.
+    '''
+    print('Finding tokens to refresh')
---
+++ square_webhooks/auth/refresh_scheduler.tf
@@ -0,0 +1,46 @@
+resource "aws_scheduler_schedule" "trigger_token_refresh" {
+  name       = "${var.env_prefix}_trigger_token_refresh"
+
+  flexible_time_window {
+    mode = "OFF"
+  }
+
+#   schedule_expression = "rate(6 hours)"
+  schedule_expression = "rate(5 minutes)"
+
+  target {
+    arn      = aws_sqs_queue.auth_queue.arn
+    role_arn = aws_iam_role.trigger_token_refresh.arn
+    input    = jsonencode({
+        action = "find_tokens_to_refresh",
+    })
+    # input = jsonencode({
+    #   MessageBody = "Greetings, programs!"
+    #   QueueUrl    = aws_sqs_queue.example.url
+    # })
+  }
+}
+
+
+resource "aws_iam_role" "trigger_token_refresh" {
+  name               = "${var.env_prefix}_auth_handler"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "scheduler.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+
+resource "aws_iam_role_policy_attachment" "sqs_write_attachment" {
+  role       = aws_iam_role.trigger_token_refresh.name
+  policy_arn = aws_iam_policy.auth_queue_sqs_write.arn
+}
+
